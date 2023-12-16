775769514a9f5f29cb7670471623d96069539998
Tyler Willingham <171991+tylerwillingham@users.noreply.github.com>
2023-12-16 15:25:35 -0800

    📝 Annotate existing models

--- components/blog/app/models/blog/post.rb
+++ components/blog/app/models/blog/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: blog_posts
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  slug       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 module Blog
   class Post < ::ApplicationRecord
     def author
