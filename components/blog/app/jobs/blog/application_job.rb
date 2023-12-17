59996dbb4e49ea3829ea04318d73f0de674d818f
Tyler Willingham <171991+tylerwillingham@users.noreply.github.com>
2023-12-16 16:14:59 -0800

    💄 Autocorrect files in components/blog

--- components/blog/app/jobs/blog/application_job.rb
+++ components/blog/app/jobs/blog/application_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Blog
   class ApplicationJob < ActiveJob::Base
   end
--- components/blog/app/models/blog/application_record.rb
+++ components/blog/app/models/blog/application_record.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Blog
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
